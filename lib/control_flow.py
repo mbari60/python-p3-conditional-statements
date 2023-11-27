322a0ec094f99fcc5b60cd4913915670e3a5706a
kevin <kevinmbari600@gmail.com>
2023-11-27 17:58:34 +0300

    assignment corrected

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -18,11 +18,11 @@ def hows_the_weather(temperature):
 
 
 def fizzbuzz(number):
-    if number % 3 == 0:
+    if number % 3 == 0 and number % 5 == 0:
         return "FizzBuzz"
-    elif number % 5 == 0:
+    elif number % 3 == 0:
         return "Fizz"
-    elif number % 3 == 0 and number % 5 == 0:
+    elif number % 5 == 0:
         return "Buzz"
     else:
         return number
